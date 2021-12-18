2874f7e69eee138d0eb013dc47af075f4f5d8f6b
Adam Stasiak <stasiak.j2000@gmail.com>
2021-12-18 18:46:06 +0100

    folder src

---
+++ src/package.cpp
@@ -0,0 +1,4 @@
+//zawierające definicje funkcjonalności odpowiednich plików nagłówkowych (oraz ewentualne definicje dodatkowych funkcjonalności,
+//niezbędnych wyłącznie w ramach danego pliku źródłowego)
+
+#include "package.hpp"
